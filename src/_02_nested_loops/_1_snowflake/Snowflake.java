630c14cf7048444a9e7182fdfa9afc4ec80838d8
neha <neha.ann.dev@gmail.com>
2024-10-09 18:27:05 -0700

    aaaaaaaaaa

--- src/_02_nested_loops/_1_snowflake/Snowflake.java
+++ src/_02_nested_loops/_1_snowflake/Snowflake.java
@@ -2,40 +2,42 @@ package _02_nested_loops._1_snowflake;
 
 import java.awt.Color;
 
+import org.jointheleague.graphical.robot.Robot;
+
 
 public class Snowflake {
 	
 	public static void main(String[] args) {
 
 		// #1. Make a new Robot
-
-
+		Robot rob = new Robot();		
 		// #2. Set your robot’s position to x=300 and y=300
-
-
+		rob.setX(300);
+		rob.setY(300);
 		// #3. Put the robot's pen down
-
-
+		rob.penDown();
 		// #4. Set the robot’s speed to 5
-
-
+		rob.setSpeed(5);
 		// #5. use a for loop below to do everything ( #6, #7, #8, #9, #12, #13, #14 ) 6 times (use i as the counter)
-
-			
+		for(int i =0;i<5;i++) {
 			// #6. Set the pen color to black
 
-			
+			rob.setPenColor(0,0,0);
 			// #7. move the robot by 50
-
+			rob.move(50);
 			
 			// #8. turn the robot to the right by 60
-			
+			rob.turn(60);
 			
 			// #12. set the pen color to blue for even numbers of i
-
+			if (i%2==0) {
+				rob.setPenColor(0,0, 10);
+			}
 			
 			// #13. set the pen color to red for all odd numbers of i
-			
+			if (i%2!=0) {
+				rob.setPenColor(10,0, 0);
+			}
 			
 			// #9. Use a for loop to do everything (#10, #11) 6 times ( use j as a counter )
 			
@@ -49,7 +51,7 @@ public class Snowflake {
 			
 			// #14. set the pen color to black
 				
-		
+		}
 	}
 
 }
