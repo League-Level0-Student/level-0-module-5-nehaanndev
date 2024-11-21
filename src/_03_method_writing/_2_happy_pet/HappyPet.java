dd7829c64768a72e2e9fef6ca2743fc6b77c6901
neha <neha.ann.dev@gmail.com>
2024-11-20 18:27:57 -0800

    s

--- src/_03_method_writing/_2_happy_pet/HappyPet.java
+++ src/_03_method_writing/_2_happy_pet/HappyPet.java
@@ -5,26 +5,47 @@ public class HappyPet {
 	
 	// 1. Add the following variable to the next line: static int happinessLevel = 0;
 	// this will be used to store the happiness of your pet
-	int happinessLevel = 0;
+	 static int happinessLevel = 0;
 	public static void main(String[] args) {
 		// 2. Ask the user what kind of pet they want to buy, and store their answer in a variable
-
+		 String petType = JOptionPane.showInputDialog("What kind of pet would you like?");
 		// 3. REPEAT steps 4 - 7 enough times to make your pet happy!
+		while ( happinessLevel <= 100) {
+			
 		
 			// 4. Use showOptionDialog to ask the user what they want to do to make their pet happy
 			//    (eg: cuddle, food, water, take a walk, groom, clean up poop).
 			//    Make sure to customize the title and question too.
-			int task = JOptionPane.showOptionDialog(null, "Question", "Title", 0, JOptionPane.INFORMATION_MESSAGE, null,
-					new String[] { "Button1", "Button2", "Button3" }, null);
+			int task = JOptionPane.showOptionDialog(null, "What do you want to make you pet enjoy life?", "Pet Happiness!", 0, JOptionPane.INFORMATION_MESSAGE, null,
+					new String[] { "groom", "talk a walk", "food", "water", "pet" }, null);
 
 			// 6. Use user input to call the appropriate method created in step 5 below.
-
+			groom(petType, happinessLevel);
+			System.out.println(happinessLevel);
 			// 7. If you determine the happiness level is large enough, tell the
 			//    user that he loves his pet and use break; to exit for loop.
+		}
+	}
 
+	private static Object happinessLevel(int i) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 	// 5. Create methods to handle each of your user selections.
 	//    Each method should create a pop-up with the pet's response (eg. cat might purr when pet), 
 	//    and INCREMENT the pet's happiness Level.
+	
+	public static int groom (String p, int happinessLevel) {
+		if (p.equals("dog")) {
+			JOptionPane.showMessageDialog(null, "Your dog's tail is wagging!");
+		} else if (p.equals("cat")) {
+			JOptionPane.showMessageDialog(null, "Your cat is purring.");
+		}else{
+			JOptionPane.showMessageDialog(null, "Your pet wants more.");
+		}
+		happinessLevel= happinessLevel + 100;
+		return happinessLevel;
+	}  
+	
 }
